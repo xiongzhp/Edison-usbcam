186c40893112d6b585395c976d51564b6b2c26ab
xiongzhp <xiongzhp150@gmail.com>
2015-06-08 00:32:07 +0800

    update

--- web/server/lib/routes/index.js
+++ web/server/lib/routes/index.js
@@ -1,7 +1,10 @@
 // Load the single view; AngularJS will handle view changes on the front-end
 // NB: This needs to be the last route added
 exports.serveIndex = function (app, staticFolder) {
-  app.get('*', function (req, res) {
+  app.get('/', function (req, res) {
     res.sendFile('index.html', { root: staticFolder });
+  });
+  app.get('/color_camera_canvas', function (req, res) {
+    res.sendFile('color_camera_canvas.html', { root: staticFolder });
   });
 };
