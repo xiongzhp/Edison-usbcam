84f771ad042b218f6b3ad360f667946ba0fbc97f
xiongzhp <xiongzhp150@gmail.com>
2015-06-07 15:06:42 +0800

    update

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b 800k -r 1 http://127.0.0.1:8082
+/home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video http://127.0.0.1:8082
 
