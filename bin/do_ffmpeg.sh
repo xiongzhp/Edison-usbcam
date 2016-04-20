7e1c957de61d13d0977dcda653e5b47ab8fdd79d
ErikXiong <xiongzhp@outlook.com>
2016-04-20 23:05:10 +0800

    update

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/home/root/bin/ffmpeg/ffmpeg -s 640x480 -f video4linux2 -i /dev/video0 -f mpeg1video -b:v 1000k -r 20 http://127.0.0.1:8082
+/home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video http://127.0.0.1:8082
